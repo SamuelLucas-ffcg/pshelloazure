1698d35cc5eecb945829a50265c06d72db5ef87a
Samuel Lucas <samuel.lucas@ffcg.se>
2019-09-06 09:37:28 +0200

    Add project files.

---
+++ pshelloazuree/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace pshelloazuree.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
