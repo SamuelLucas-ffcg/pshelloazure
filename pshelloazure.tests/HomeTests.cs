dfb793f687ab58dffdd10277a13d910cf0e1c5b4
Samuel Lucas <samuel.lucas@ffcg.se>
2019-09-12 16:13:28 +0200

    Adding Tests to HomeController

---
+++ pshelloazure.tests/HomeTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using NUnit.Framework;
+using pshelloazuree.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pshelloazure.tests
+{
+    [TestFixture]
+    class HomeTests
+    {
+        private Mock<IConfiguration> config;
+        [SetUp]
+        public void SetUp()
+        {
+            config = new Mock<IConfiguration>();
+        }
+        [Test]
+        public void TestActionReturnsExeption()
+        {
+            var controller = new HomeController(config.Object);
+
+            Assert.Throws<InvalidOperationException>(() => controller.Test());
+        }
+    }
+}
